b8c859a5e1d577d4610ea4c959e0d7739079fc4b
Nathan Nguyen <157441039+CvmuloSky@users.noreply.github.com>
2024-09-20 09:09:50 -0400

    New Map

--- PLayer.gd
+++ PLayer.gd
@@ -3,7 +3,7 @@ extends VehicleBody3D
 const MAX_STEER = .8
 const ENGINE_POWER = 500
 var normal_friction_slip = 1.0
-var drift_friction_slip = 0.8
+var drift_friction_slip = 0.68
 var is_drifting = false
 
 @onready var camera_pivot = $CameraPivot
