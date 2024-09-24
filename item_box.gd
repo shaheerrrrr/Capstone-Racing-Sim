27b293ad22128203ea9710e8455639f7014b559c
Shaheer Khan <96140967+shaheerrrrr@users.noreply.github.com>
2024-09-24 09:39:10 -0400

    Update item_box.gd

--- item_box.gd
+++ item_box.gd
@@ -29,32 +29,100 @@ func _2ndPlaceBoxRoulette() -> String:
 	var rand = randf()
 	if (rand <.175): # 17.5% chance for a green shell
 		return "Green Shell"
-	elif (rand < .375): # 25% chance for a red shell
+	elif (rand < .425): # 25% chance for a red shell
 		return "Red Shell"
-	elif (rand < .452): # 37.5% chance for a banana
+	elif (rand < .625): # 20% chance for a banana
 		return "Banana"
-	elif (rand < .525): # 5% chance for a fake item box (may be removed from game)
+	elif (rand < .700): # 7.5% chance for a fake item box (may be removed from game)
 		return "Fake Item Box"
-	elif (rand < .700): # 10% chance for a mushroom
+	elif (rand < .825): # 12.5% chance for a mushroom
 		return "Mushroom"
-	elif (rand < .725): # 14% chance for a triple mushroom
-		return "Triple Mushroom"
-	elif (rand < .750):
-		return "Bobomb"
-	else: # 1% chance for a mushroom
-		return "Mushroom"
+	elif (rand < .875): # 5% chance for a triple green shells
+		return "Triple Green Shells"
+	else: # 15% chance for a triple banana
+		return "Triple Banana"
 	
 
 func _3rdPlaceBoxRoulette() -> String:
-	
+	var rand = randf()
+	if (rand <.150): # 15% chance for a green shell
+		return "Green Shell"
+	elif (rand < .400): # 25% chance for a red shell
+		return "Red Shell"
+	elif (rand < .475): # 7.5% chance for a banana
+		return "Banana"
+	elif (rand < .525): # 5% chance for a fake item box (may be removed later)
+		return "Fake Item Box"
+	elif (rand < .700): # 17.5% chance for a mushroom
+		return "Mushroom"
+	elif (rand < .725): # 2.5% chance for a bobomb
+		return "Bobomb"
+	elif (rand < .750): # 2.5% chance for a thundercloud
+		return "Thundercloud"
+	elif (rand < .850): # 10% chance for a triple green shells
+		return "Triple Green Shells"
+	elif (rand < .900): # 5% chance for a triple red shells
+		return "Triple Red Shells"
+	else: # 10% chance for a triple banana
+		return "Triple Banana"
 	
 
 func _4thPlaceBoxRoulette() -> String:
-	
+	var rand = randf()
+	if (rand <.075): # 7.5% chance for a green shell
+		return "Green Shell"
+	elif (rand < .275): # 20% chance for a red shell
+		return "Red Shell"
+	elif (rand < .300): # 2.5% chance for a banana
+		return "Banana"
+	elif (rand < .325): # 2.5% chance for a fake item box (may be removed later)
+		return "Fake Item Box"
+	elif (rand < .550): # 22.5% chance for a mushroom
+		return "Mushroom"
+	elif (rand < .600): # 5% chance for a triple mushroom
+		return "Triple Mushroom"
+	elif (rand < .650): # 5% chance for a bobomb
+		return "Bobomb"
+	elif (rand < .675): # 2.5% chance for a blue shell
+		return "Blue Shell"
+	elif (rand < .700): # 2.5% chance for a mega mushroom
+		return "Mega Mushroom"
+	elif (rand < .775): # 7.5% chance for a thundercloud
+		return "Thundercloud"
+	elif (rand < .875): # 10% chance for a triple green shells
+		return "Triple Green Shells"
+	elif (rand < .975): # 10% chance for a triple red shells
+		return "Triple Red Shells"
+	else: # 2.5% chance for a triple banana
+		return "Triple Banana"
 	
 
 func _5thPlaceBoxRoulette() -> String:
-	
+	var rand = randf()
+	if (rand <.050): # 5% chance for a green shell
+		return "Green Shell"
+	elif (rand < .200): # 20% chance for a red shell
+		return "Red Shell"
+	elif (rand < .350): # 15% chance for a mushroom
+		return "Mushroom"
+	elif (rand < .450): # 10% chance for a triple mushroom
+		return "Triple Mushroom"
+	elif (rand < .525): # 7.5% chance for a bobomb
+		return "Bobomb"
+	elif (rand < .575): # 5% chance for a blue shell
+		return "Blue Shell"
+	elif (rand < .650): # 7.5% chance for a mega mushroom
+		return "Mega Mushroom"
+	elif (rand < .700): # 7.5% chance for a milker (renamed blooper)
+		return "Blooper"
+	elif (rand < .775): # 7.5% chance for a triple green shells
+		return "Thundercloud"
+	elif (rand < .875): # 12.5% chance for a triple green shells
+		return "Triple Green Shells"
+	elif (rand < .975): # 10% chance for a triple red shells
+		return "Triple Red Shells"
+	else: # 2.5% chance for a triple banana
+		return "Triple Banana"
 	
 
 func _6thPlaceBoxRoulette() -> String:
